89bb4d55d66a2c89c9f48f5043edf5f0979fdacd
Jhonny Ribeiro <jaribeiro32@gmail.com>
2020-01-20 19:30:58 -0300

    Full version hansOn only one commit, simple verbs save, delete, get, push, patch

---
+++ routes/api.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+Route::namespace('Api')->prefix('products')->group(function(){
+
+    Route::get('/', 'ProductController@index');
+    Route::get('/{id}', 'ProductController@show');
+    Route::post('/', 'ProductController@save');
+    Route::put('/', 'ProductController@update');
+    Route::patch('/', 'ProductController@update');
+    Route::delete('/{id}', 'ProductController@delete');
+
+});
